96d67ba255de897a6e4ca0cbebee2c901ec2a167
v99 <145901050+KoyaHoe@users.noreply.github.com>
2025-04-21 19:11:38 -0600

    Update and rename Main Script to Main Script.lua

--- Main Script.lua
+++ Main Script.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("soon"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/KoyaHoe/WilsonHub/main/GamesSupported.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
