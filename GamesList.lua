a1c96b187a8593aa35b3dc575f1fbd6ab909dfdd
v99 <145901050+KoyaHoe@users.noreply.github.com>
2025-05-07 15:24:38 -0600

    Update GamesList.lua

--- GamesList.lua
+++ GamesList.lua
@@ -1,4 +1,5 @@
 local Games = {
+  [87039211657390] = "https://raw.githubusercontent.com/KoyaHoe/Game-Scripts/main/Arise%20Crossover.lua"
   [3623096087] = "Actualizo Luego", -- Muscle Legends
   [537413528] = "https://raw.githubusercontent.com/KoyaHoe/Game-Scripts/main/Build%20A%20Boat%20For%20Treasure.lua", -- Build A Boat For Treasure
 }
