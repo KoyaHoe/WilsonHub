7c3a4e6c9d166c54f55ba844c3bcdae200c7d69c
v99 <145901050+KoyaHoe@users.noreply.github.com>
2025-05-08 09:35:53 -0600

    Update GamesList.lua

--- GamesList.lua
+++ GamesList.lua
@@ -1,7 +1,6 @@
 local Games = {
   [87039211657390] = "https://raw.githubusercontent.com/KoyaHoe/Game-Scripts/main/Arise%20Crossover.lua", -- Arise Crossover
   [128336380114944] = "https://raw.githubusercontent.com/KoyaHoe/Game-Scripts/main/Arise%20Crossover.lua", -- Dungeon Arise Crossover
-  [128336380114944] = "https://raw.githubusercontent.com/KoyaHoe/Game-Scripts/main/Arise%20Crossover.lua", -- Infernal Castle Arise Crossover
   [3623096087] = "Actualizo Luego", -- Muscle Legends
   [537413528] = "https://raw.githubusercontent.com/KoyaHoe/Game-Scripts/main/Build%20A%20Boat%20For%20Treasure.lua", -- Build A Boat For Treasure
 }
